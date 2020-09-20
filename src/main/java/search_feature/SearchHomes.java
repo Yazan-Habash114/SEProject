ff1075b3d703c5563b2d5b5e6fe5ff6b6c197508
Yazan-Habash114 <54286064+Yazan-Habash114@users.noreply.github.com>
2020-09-20 21:43:21 +0300

    Update 'SearchHomes.java'

--- src/main/java/search_feature/SearchHomes.java
+++ src/main/java/search_feature/SearchHomes.java
@@ -4,13 +4,15 @@ import java.util.ArrayList;
 
 public class SearchHomes {
 	
-	ArrayList <Home> homes;
-	ArrayList <Home> found_homes;
+	private ArrayList <Home> homes;
+	private ArrayList <Home> found_homes;
+	private boolean combinational;
 	
 	//Constructor
 	public SearchHomes() {
 		homes = new ArrayList <Home>();
 		found_homes = new ArrayList <Home>();
+		combinational = false;
 	}
 	
 	// Methods
@@ -21,6 +23,8 @@ public class SearchHomes {
 	public void printFoundHomes() {
 		for(Home h: found_homes)
 			System.out.println(h.toString());
+		found_homes.clear();
+		combinational = false;
 	}
 
 	public void searchBy(String string) {
