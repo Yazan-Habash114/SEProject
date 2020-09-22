aae93d659923cf75c5a5f9e4c767b7b1f8b2ca01
OTHMAN\othman <othman.subhi.123@gmail.com>
2020-09-22 19:04:13 +0300

    scenarios print all results

--- src/test/java/project/testing/SearchSteps.java
+++ src/test/java/project/testing/SearchSteps.java
@@ -40,85 +40,96 @@ public class SearchSteps {
 	//scenario 1 
 	@Then("A list of homes that match the placment specification should be returned and printed on the console")
 	public void aListOfHomesThatMatchThePlacmentSpecificationShouldBeReturnedAndPrintedOnTheConsole() {
-	    
+	    sh.printFoundHomes();
+	    return;
 	}
 	
 	
 	//scenario 2
 	@Then("A list of homes that match the  Material specification should be returned and printed on the console")
 	public void aListOfHomesThatMatchTheMaterialSpecificationShouldBeReturnedAndPrintedOnTheConsole() {
-	    
+		sh.printFoundHomes();
+	    return;
 	}
 	
 	
 	//scenario 3
 	@Then("A list of homes that are below a specified price should be returned and printed on the console")
 	public void aListOfHomesThatAreBelowASpecifiedPriceShouldBeReturnedAndPrintedOnTheConsole() {
-	    
+		sh.printFoundHomes();
+	    return;
 	}
 	
 	
 	//scenario 4
 	@Then("A list of homes that are in the price range should be returned and printed on the console")
 	public void aListOfHomesThatAreInThePriceRangeShouldBeReturnedAndPrintedOnTheConsole() {
-	    
+		sh.printFoundHomes();
+	    return;
 	}
 	
 	
 	//scenario 5
 	@Then("A list of homes that below a specified area should be returned and printed on the console")
 	public void aListOfHomesThatBelowASpecifiedAreaShouldBeReturnedAndPrintedOnTheConsole() {
-	    
+		sh.printFoundHomes();
+	    return;
 	}
 	
 	
 	//scenario 6
 	@Then("A list of homes that in the Area range should be returned and printed on the console")
 	public void aListOfHomesThatInTheAreaRangeShouldBeReturnedAndPrintedOnTheConsole() {
-	    
+		sh.printFoundHomes();
+	    return;
 	}
 	
 	
 	//scenario 7
 	@Then("A list of homes that match the Number of bedrooms should be returned and printed on the console")
 	public void aListOfHomesThatMatchTheNumberOfBedroomsShouldBeReturnedAndPrintedOnTheConsole() {
-	    
+		sh.printFoundHomes();
+	    return;
 	}
 	
 	
 	//scenario 8
 	@Then("A list of homes that match the Number of bathrooms should be returned and printed on the console")
 	public void aListOfHomesThatMatchTheNumberOfBathroomsShouldBeReturnedAndPrintedOnTheConsole() {
-	    
+		sh.printFoundHomes();
+	    return;
 	}
 	
 	
 	//scenario 9
 	@Then("A list of homes that are Allowing pets should be returned and printed on the console")
 	public void aListOfHomesThatAreAllowingPetsShouldBeReturnedAndPrintedOnTheConsole() {
-	    
+		sh.printFoundHomes();
+	    return;
 	}
 	
 	
 	//scenario 10
 	@Then("A list of homes that match the  type specification should be returned and printed on the console")
 	public void aListOfHomesThatMatchTheTypeSpecificationShouldBeReturnedAndPrintedOnTheConsole() {
-	    
+		sh.printFoundHomes();
+	    return;
 	}
 	
 	
 	//scenario 11
 	@Then("A list of homes that match the lease length period should be returned and printed on the console")
 	public void aListOfHomesThatMatchTheLeaseLengthPeriodShouldBeReturnedAndPrintedOnTheConsole() {
-	    
+		sh.printFoundHomes();
+	    return;
 	}
 	
 	
 	//scenario 12
 	@Then("A list of homes that provid the Amenities specified should be returned and printed on the console")
 	public void aListOfHomesThatProvidTheAmenitiesSpecifiedShouldBeReturnedAndPrintedOnTheConsole() {
-	    
+		sh.printFoundHomes();
+	    return;
 	}
-
 	
 }
